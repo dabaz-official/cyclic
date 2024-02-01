f7200cad46d94577af14963917187d6d964d0a9b
DabAZ <dabaz@dabaz.me>
2024-02-02 01:46:04 +0800

    Setup folders

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+const RootLayout = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+}
+ 
+export default RootLayout;
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@cyclic/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center text-center p-24">
+      <p className="text-5xl font-bold mb-6">
+        Your Minimalist Note-Taking App
+      </p>
+      <Button>
+        Enjoy Cyclic -&gt;
+      </Button>
+    </main>
+  );
+}
