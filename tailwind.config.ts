628bdfd6af580f8a1354690ad317dff2ca05da6f
DabAZ <dabaz@dabaz.me>
2024-02-02 02:19:27 +0800

    Add fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,8 @@ const config = {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
+        mono: ["var(--font-mono)", ...fontFamily.mono],
+        serif: ["var(--font-serif)", ...fontFamily.serif],
       },
       colors: {
         border: "hsl(var(--border))",
