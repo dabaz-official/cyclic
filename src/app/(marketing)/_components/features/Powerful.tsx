75aa1cedcdaa2505af2de50b58069a460cc91b87
DabAZ <dabaz@dabaz.me>
2024-02-02 11:26:29 +0800

    Create Powerful.tsx

---
+++ src/app/(marketing)/_components/features/Powerful.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export const Powerful = () => {
+  return (
+    <div className="max-w-3xl space-y-6 px-8 sm:px-20 pt-16 md:pt-12">
+      <div className="bg-white px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-4xl sm:text-5xl font-bold tracking-tight text-neutral-900">Simple but <span className="text-blue-600">powerful</span></h2>
+          <p className="mt-6 text-md sm:text-lg text-neutral-900">
+            We keep everything simple while we are making Cyclic powerful for the best notes solution.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
