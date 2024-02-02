6fcdf9100157830a442d29f4775e3dd9aff2053e
DabAZ <dabaz@dabaz.me>
2024-02-02 15:52:51 +0800

    Modify hero section

--- src/app/(marketing)/_components/Header.tsx
+++ src/app/(marketing)/_components/Header.tsx
@@ -1,27 +1,6 @@
-import Image from "next/image";
-
-import { Button } from "@cyclic/components/ui/button";
-
-export const MarketingHeader = () => {
+export default function MarketingHeader() {
   return (
     <header className="bg-white">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <a href="/" className="-m-1.5 p-1.5">
-          <span className="sr-only">Cyclic</span>
-          <Image
-            className="h-8 w-auto"
-            src="/images/logos/logo-light.svg"
-            width={8}
-            height={8}
-            alt="Logo"
-          />
-        </a>
-        <div className="hidden md:flex md:gap-x-12">
-          <Button size="sm" className="text-sm leading-6">
-            Enter Cyclic
-          </Button>
-        </div>
-      </nav>
-    </header>  
+    </header>
   )
 }
--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,18 +1,16 @@
-import { MarketingBanner } from "./_components/Banner";
-import { MarketingHeading } from "./_components/Heading";
+import Cookies from "./_components/Cookies";
+import MarketingFeatures from "./_components/features/Features";
 import { Hero } from "./_components/Hero";
-import { MarketingHeader } from "./_components/Header";
-import { Powerful } from "./_components/features/Powerful";
 
 export default function MarketingPage() {
   return (
     <main className="flex min-h-screen flex-col">
-      <MarketingBanner />
-      <MarketingHeader />
-      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
-        <MarketingHeading />
-        <Hero />
-        <Powerful />
+      <Cookies />
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-3xl flex flex-col items-center justify-center md:justify-start text-center py-16 gap-y-24 flex-1">
+          <Hero />
+          <MarketingFeatures />
+        </div>
       </div>
     </main>
   );
