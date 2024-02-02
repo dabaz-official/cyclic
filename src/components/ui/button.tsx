bb33a85e9a5c89559cd9cb25ce864ce77c8e3efd
DabAZ <dabaz@dabaz.me>
2024-02-02 11:26:15 +0800

    Add hero section

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        orange: "bg-orange-600 text-orange-100 hover:bg-orange-600/90",
       },
       size: {
         default: "h-10 px-4 py-2",
