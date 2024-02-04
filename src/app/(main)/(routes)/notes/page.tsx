374db99cd2fb77a6c06f67e6a3bac5c14fb15f8f
DabAZ <dabaz@dabaz.me>
2024-02-04 09:29:37 +0800

    Modify the note page

--- src/app/(main)/(routes)/notes/page.tsx
+++ src/app/(main)/(routes)/notes/page.tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
+import { Button } from "@cyclic/components/ui/button";
+import { Plus } from "lucide-react";
+
 const NotesPage = () => {
+
   return (
-    <div>
-      This is a protected page.
+    <div className="h-full min-h-screen flex flex-col items-center justify-center">
+      <Image
+        src="/images/notes/empty-light.svg"
+        height="200"
+        width="200"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/images/notes/empty-dark.svg"
+        height="200"
+        width="200"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-2xl font-bold tracking-tight mt-4">
+        Welcome to Cyclic.
+      </h2>
+      <Button className="mt-4">
+        <Plus className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
     </div>
   );
 }
