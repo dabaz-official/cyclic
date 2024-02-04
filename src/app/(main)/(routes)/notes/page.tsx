3ab11a68c13e7238eb687028d8143878b00802b6
DabAZ <dabaz@dabaz.me>
2024-02-04 17:05:02 +0800

    Add create note function

--- src/app/(main)/(routes)/notes/page.tsx
+++ src/app/(main)/(routes)/notes/page.tsx
@@ -1,8 +1,29 @@
+"use client";
+
 import Image from "next/image";
-import { Button } from "@cyclic/components/ui/button";
 import { Plus } from "lucide-react";
+import { useMutation } from "convex/react";
+import { useUser } from "@clerk/clerk-react";
+import { toast } from "sonner";
+
+import { api } from "../../../../../convex/_generated/api"; 
+import { Button } from "@cyclic/components/ui/button";
 
 const NotesPage = () => {
+  const { user } = useUser();
+  const create = useMutation(api.pages.createPage)
+
+  const onCreate = () => {
+    const promise = create({ 
+      title: "Untitled"
+    });
+
+    toast.promise(promise, {
+      loading: "Creating a new note for you...",
+      success: "New note created!",
+      error: "Failed to create a new note."
+    })
+  }
 
   return (
     <div className="h-full min-h-screen flex flex-col items-center justify-center">
@@ -23,7 +44,7 @@ const NotesPage = () => {
       <h2 className="text-2xl font-bold tracking-tight mt-4">
         Welcome to Cyclic.
       </h2>
-      <Button className="mt-4">
+      <Button onClick={onCreate} className="mt-4">
         <Plus className="h-4 w-4 mr-2" />
         Create a note
       </Button>
