9a724408ee43eafb5869ef88594f703d97503961
DabAZ <dabaz@dabaz.me>
2024-04-15 18:49:57 +0800

    Add title display

--- src/components/page/toolbar.tsx
+++ src/components/page/toolbar.tsx
@@ -3,6 +3,7 @@
 import { ElementRef, useRef, useState } from "react";
 import { ImagePlus, SmilePlus, X } from "lucide-react";
 import { useMutation } from "convex/react";
+import TextAreaAutosize from "react-textarea-autosize";
 
 import { Doc } from "../../../convex/_generated/dataModel";
 import { Button } from "@cyclic/components/ui/button";
@@ -35,6 +36,23 @@ export const Toolbar = ({
   };
 
   const disableInput = () => setIsEditing(false);
+
+  const onInput = (value: string) => {
+    setValue(value);
+    update({
+      id: initialData._id,
+      title: value || "Untitled"
+    });
+  };
+
+  const onKeyDown = (
+    event: React.KeyboardEvent<HTMLTextAreaElement>
+  ) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      disableInput();
+    }
+  };
 
   return (
     <div className="pl-[54px] group relative">
@@ -85,6 +103,23 @@ export const Toolbar = ({
           </Button>
         )}
       </div>
+      {!isEditing && !preview ? (
+        <TextAreaAutosize
+          ref={inputRef}
+          onBlur={disableInput}
+          onKeyDown={onKeyDown}
+          value={value}
+          onChange={(e) => onInput(e.target.value)}
+          className="text-5xl bg-transparent font-bold break-words outline-none text-neutral-700 dark:text-neutral-300 resize-none"
+        />
+      ) : (
+        <div
+          onClick={enableInput}
+          className="pb-[11.5px] text-5xl font-bold break-words outline-none text-neutral-700 dark:text-neutral-300"
+        >
+          {initialData.title}
+        </div>
+      )}
     </div>
   )
 }
