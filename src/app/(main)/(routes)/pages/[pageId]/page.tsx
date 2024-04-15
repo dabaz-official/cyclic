e7725ba0c8eef1b17ea1a8cbdbb5fc924c030f20
DabAZ <dabaz@dabaz.me>
2024-04-16 01:00:21 +0800

    Add skeleton

--- src/app/(main)/(routes)/pages/[pageId]/page.tsx
+++ src/app/(main)/(routes)/pages/[pageId]/page.tsx
@@ -6,6 +6,7 @@ import { api } from "../../../../../../convex/_generated/api";
 import { Id } from "../../../../../../convex/_generated/dataModel";
 import { Toolbar } from "@cyclic/components/page/toolbar";
 import { Cover } from "@cyclic/components/page/cover";
+import { Skeleton } from "@cyclic/components/ui/skeleton";
 
 interface PageIdPageProps {
   params: {
@@ -23,7 +24,15 @@ const PageIdPage = ({
   if (page === undefined) {
     return (
       <div>
-        Loading...
+        <Cover.Skeleton />
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-4 w-[80%]" />
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-4 w-[60%]" />
+          </div>
+        </div>
       </div>
     );
   };
--- src/components/page/cover.tsx
+++ src/components/page/cover.tsx
@@ -11,6 +11,7 @@ import { useCoverImage } from "@cyclic/hooks/use-cover-image";
 import { api } from "../../../convex/_generated/api";
 import { Id } from "../../../convex/_generated/dataModel";
 import { useEdgeStore } from "@cyclic/lib/edgestore";
+import { Skeleton } from "@cyclic/components/ui/skeleton";
 
 interface CoverImageProps {
   url?: string;
@@ -75,5 +76,11 @@ export const Cover = ({
         </div>
       )}
     </div>
+  )
+}
+
+Cover.Skeleton = function CoverSkeleton() {
+  return (
+    <Skeleton className="w-full h-[12vh]" />
   )
 }
