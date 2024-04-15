edd3a69b803d5305db8261011adfb27376747542
DabAZ <dabaz@dabaz.me>
2024-04-16 00:12:26 +0800

    Add change cover functionality

--- src/components/page/cover.tsx
+++ src/components/page/cover.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import { cn } from "@cyclic/lib/utils";
 import Image from "next/image";
+import { ImageIcon, ImageOff } from "lucide-react";
+
+import { cn } from "@cyclic/lib/utils";
+import { Button } from "@cyclic/components/ui/button";
+import { useCoverImage } from "@cyclic/hooks/use-cover-image";
 
 interface CoverImageProps {
   url?: string;
@@ -12,6 +16,8 @@ export const Cover = ({
   url,
   preview,
 }: CoverImageProps) => {
+  const coverImage = useCoverImage();
+
   return (
     <div className={cn(
       "relative w-full h-[35vh] group",
@@ -25,6 +31,28 @@ export const Cover = ({
           alt="Cover"
           className="object-cover"
         />
+      )}
+      {url && !preview && (
+        <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+          <Button
+            onClick={coverImage.onOpen}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            Change cover
+          </Button>
+          <Button
+            onClick={() => {}}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageOff className="h-4 w-4 mr-2" />
+            Remove cover
+          </Button>
+        </div>
       )}
     </div>
   )
