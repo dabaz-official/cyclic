ac6d912a51ce3def13af85aa342b62e2205b7ee7
DabAZ <dabaz@dabaz.me>
2024-04-15 17:48:18 +0800

    Add toolbar

--- src/app/(main)/(routes)/pages/[pageId]/page.tsx
+++ src/app/(main)/(routes)/pages/[pageId]/page.tsx
@@ -1,9 +1,47 @@
-const PageIdPage = () => {
+"use client";
+
+import { useQuery } from "convex/react";
+
+import { api } from "../../../../../../convex/_generated/api";
+import { Id } from "../../../../../../convex/_generated/dataModel";
+import { Toolbar } from "@cyclic/components/page/toolbar";
+
+interface PageIdPageProps {
+  params: {
+    pageId: Id<"pages">;
+  };
+};
+
+const PageIdPage = ({
+  params
+}: PageIdPageProps) => {
+  const page = useQuery(api.pages.getById, {
+    pageId: params.pageId,
+  });
+
+  if (page === undefined) {
+    return (
+      <div>
+        Loading...
+      </div>
+    );
+  };
+
+  if (page === null)
   return (
     <div>
-      <h1>PageIdPage</h1>
+      Page not found.
     </div>
   );
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={page} />
+      </div>
+    </div>
+  )
 }
 
 export default PageIdPage;
