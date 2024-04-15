eb15c92be960d4bec9b0e17921dd7711eee996ae
DabAZ <dabaz@dabaz.me>
2024-04-15 20:19:25 +0800

    Add cover image component

--- src/app/(main)/(routes)/pages/[pageId]/page.tsx
+++ src/app/(main)/(routes)/pages/[pageId]/page.tsx
@@ -5,6 +5,7 @@ import { useQuery } from "convex/react";
 import { api } from "../../../../../../convex/_generated/api";
 import { Id } from "../../../../../../convex/_generated/dataModel";
 import { Toolbar } from "@cyclic/components/page/toolbar";
+import { Cover } from "@cyclic/components/page/cover";
 
 interface PageIdPageProps {
   params: {
@@ -36,7 +37,7 @@ const PageIdPage = ({
 
   return (
     <div className="pb-40">
-      <div className="h-[35vh]" />
+      <Cover />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={page} />
       </div>
---
+++ src/components/page/cover.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+interface CoverImageProps {
+  url?: string;
+  preview?: boolean;
+}
+
+export const Cover = ({
+  url,
+  preview,
+}: CoverImageProps) => {
+  return (
+    <div></div>
+  )
+}
