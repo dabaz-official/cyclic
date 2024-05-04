d03c91b28955918d71e3ecbdfc16d6196014cd3d
DabAZ <dabaz@dabaz.me>
2024-05-05 03:09:35 +0800

    chore: remove settings

--- src/app/(main)/_components/navigation.tsx
+++ src/app/(main)/_components/navigation.tsx
@@ -8,7 +8,7 @@ import {
   Plus,
   Search,
   Settings,
-  Trash
+  Trash,
 } from "lucide-react";
 import { useParams, usePathname, useRouter } from "next/navigation";
 import { useMediaQuery } from "usehooks-ts";
@@ -20,7 +20,7 @@ import { api } from "../../../../convex/_generated/api";
 import {
   Popover,
   PopoverTrigger,
-  PopoverContent
+  PopoverContent,
 } from "@cyclic/components/ui/popover";
 import { Item } from "./item";
 import { toast } from "sonner";
@@ -51,16 +51,16 @@ export const Navigation = () => {
     } else {
       resetWidth();
     }
-  }, [isMobile])
+  }, [isMobile]);
 
   useEffect(() => {
     if (isMobile) {
       collapse();
     }
-  }, [pathname, isMobile])
+  }, [pathname, isMobile]);
 
   const handleMouseDown = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
   ) => {
     event.preventDefault();
     event.stopPropagation();
@@ -80,7 +80,10 @@ export const Navigation = () => {
     if (sidebarRef.current && navbarRef.current) {
       sidebarRef.current.style.width = `${newWidth}px`;
       navbarRef.current.style.setProperty("left", `${newWidth}px`);
-      navbarRef.current.style.setProperty("width", `calc(100% - ${newWidth}px)`);
+      navbarRef.current.style.setProperty(
+        "width",
+        `calc(100% - ${newWidth}px)`,
+      );
     }
   };
 
@@ -98,12 +101,9 @@ export const Navigation = () => {
       sidebarRef.current.style.width = isMobile ? "100%" : "240px";
       navbarRef.current.style.setProperty(
         "width",
-        isMobile ? "0" : "calc(100% - 240px)"
+        isMobile ? "0" : "calc(100% - 240px)",
       );
-      navbarRef.current.style.setProperty(
-        "left",
-        isMobile ? "100%" : "240px"
-      );
+      navbarRef.current.style.setProperty("left", isMobile ? "100%" : "240px");
       setTimeout(() => setIsResetting(false), 300);
     }
   };
@@ -118,16 +118,17 @@ export const Navigation = () => {
       navbarRef.current.style.setProperty("left", "0");
       setTimeout(() => setIsResetting(false), 300);
     }
-  }
+  };
 
   const handleCreate = () => {
-    const promise = create({ title: "Untitled" })
-      .then((pageId) => router.push(`/pages/${pageId}`))
+    const promise = create({ title: "Untitled" }).then((pageId) =>
+      router.push(`/pages/${pageId}`),
+    );
 
     toast.promise(promise, {
       loading: "Creating a new page for you...",
       success: "New page created!",
-      error: "Failed to create a new page."
+      error: "Failed to create a new page.",
     });
   };
 
@@ -138,43 +139,25 @@ export const Navigation = () => {
         className={cn(
           "group/sidebar min-h-screen bg-secondary overflow-y-auto relative flex w-60 flex-col z-[89999]",
           isResetting && "transition-all ease-in-out duration-300",
-          isMobile && "w-0"
+          isMobile && "w-0",
         )}
       >
         <UserItem />
-        <Item
-          label="Search"
-          icon={Search}
-          isSearch
-          onClick={search.onOpen}
-        />
-        <Item
-          label="Settings"
-          icon={Settings}
-          onClick={settings.onOpen}
-        />
-        <Item
-          onClick={handleCreate}
-          label="New page"
-          icon={FilePlus}
-        />
+        <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
+        <Item onClick={handleCreate} label="New page" icon={FilePlus} />
         <div
           onClick={collapse}
           role="button"
           className={cn(
             "h-8 w-8 text-muted-foreground rounded-sm hover:bg-neutral-200 dark:hover:bg-neutral-700 absolute top-3 right-3 opacity-0 group-hover/sidebar:opacity-100 transition p-1",
-            isMobile && "opacity-100"
+            isMobile && "opacity-100",
           )}
         >
           <PanelLeftClose className="h-6 w-6" />
         </div>
         <div className="mt-4">
           <PageList />
-          <Item
-            onClick={handleCreate}
-            icon={Plus}
-            label="Add a page"
-          />
+          <Item onClick={handleCreate} icon={Plus} label="Add a page" />
           <Popover>
             <PopoverTrigger className="w-full mt-4">
               <Item label="Trash" icon={Trash} />
@@ -198,20 +181,24 @@ export const Navigation = () => {
         className={cn(
           "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
           isResetting && "transition-all ease-in-out duration-300",
-          isMobile && "left-0 w-full"
+          isMobile && "left-0 w-full",
         )}
       >
         {!!params.pageId ? (
-          <Navbar
-            isCollapsed={isCollapsed}
-            onResetWidth={resetWidth}
-          />
+          <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
         ) : (
           <nav className="bg-transparent px-3 py-2 w-full">
-            {isCollapsed && <PanelLeftOpen onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />}
+            {isCollapsed && (
+              <PanelLeftOpen
+                onClick={resetWidth}
+                role="button"
+                className="h-6 w-6 text-muted-foreground"
+              />
+            )}
           </nav>
         )}
       </div>
     </>
-  )
-}
+  );
+};
+
