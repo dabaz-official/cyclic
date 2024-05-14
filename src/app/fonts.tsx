6db5adaad83275465c743e153b7182a2e868229a
DabAZ <dabaz@dabaz.me>
2024-05-14 15:29:53 +0800

    style: change the font

--- src/app/fonts.tsx
+++ src/app/fonts.tsx
@@ -1,10 +1,51 @@
-import { Inter as FontSans } from "next/font/google";
 import { Inconsolata as FontMono } from "next/font/google";
 import { Playfair_Display as FontSerif } from "next/font/google";
+import localFont from "next/font/local";
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
+export const fontSans = localFont({
   variable: "--font-sans",
+  src: [
+    {
+      path: './fonts/test-soehne-buch.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/test-soehne-buch-kursiv.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './fonts/test-soehne-kraftig.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/test-soehne-kraftig-kursiv.woff2',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: './fonts/test-soehne-halbfett.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/test-soehne-halbfett-kursiv.woff2',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: './fonts/test-soehne-dreiviertelfett.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/test-soehne-dreiviertelfett-kursiv.woff2',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
 })
 
 export const fontMono = FontMono({
